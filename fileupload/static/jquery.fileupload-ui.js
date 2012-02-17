b5953303dbcc00e942d736c91b8c390ebadcbe22
Ari Hershowitz <aih@tabulaw.com>
2012-02-16 17:32:25 -0800

    Increased file size

--- fileupload/static/jquery.fileupload-ui.js
+++ fileupload/static/jquery.fileupload-ui.js
@@ -29,7 +29,7 @@
             // allowed to be uploaded using this widget:
             maxNumberOfFiles: 5,
             // The maximum allowed file size:
-            maxFileSize: 1000000,
+            maxFileSize: 4000000,
             // The minimum allowed file size:
             minFileSize: 1,
             // The regular expression for allowed file types, matches
