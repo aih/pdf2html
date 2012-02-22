073f40d95cc00e3eb1514360c20bbb828f5187e9
Ari Hershowitz <aih@tabulaw.com>
2012-02-21 21:50:57 -0800

    removed <p> from cleaned txt

--- fileupload/utils/fileconvert.py
+++ fileupload/utils/fileconvert.py
@@ -1,6 +1,6 @@
 import os
 from lxml.html.clean import clean_html
-import subprocess
+import subprocess, re
 
 def convertpdf2html(pdffilepath):
     subprocess.call(['pdftotext', '-layout', pdffilepath])
@@ -9,10 +9,12 @@ def convertpdf2html(pdffilepath):
         txt = filename.read()
     #txt = unicode(txt, "utf-8", errors = 'ignore')
     txt = clean_html(txt)
+    txt = re.sub('^<p>','',txt)
+    txt = re.sub('</p>$','',txt)
 
     with open(txtfilepath, 'wb') as filename:
         filename.write(txt)
-    p = subprocess.Popen(['txt2html', '--explicitheadings', 'indentparbreak', 'make_anchors', '--tables', '-8', '--xhtml', txtfilepath ], bufsize=-1, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr= subprocess.PIPE, close_fds = True)
+    p = subprocess.Popen(['txt2html', '--explicit_headings', 'indent_par_break', 'make_anchors', '--tables', '-8', '--xhtml', txtfilepath ], bufsize=-1, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr= subprocess.PIPE, close_fds = True)
     htmltext, stderr_text = p.communicate() 
     return htmltext
 
